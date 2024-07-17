ae4126611bdabac2d79737d4feaa51d41ec2230b
Zeke Tinch <zeketinch@gmail.com>
2024-07-17 18:43:13 -0500

    updated the readme file and completed the routes

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # note-taker
 
-i made a server.js file to work in for the majority of the assignment
+I made a server.js file to make and complete my routes.
+I used the app.get to collect the information and to send the information.
+app.post was used to make unique ID's for each note.
+The app.delete mixed with the fs was used to complete the delete function.
 
 ![alt text](note-taker.png)
 
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
-const api = require('./public/assets/js/index.js');
+const fs = require('fs');
+const { error } = require('console');
+const uniqid = require('uniqid');
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,7 +10,6 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -16,9 +17,46 @@ app.get('/notes', (req, res) =>
 res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        res.send(data);
+    })
+})
+
+app.post('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        const notes = JSON.parse(data)
+        const  newNote = req.body
+        newNote.id = uniqid()
+        notes.push(newNote)
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+            res.json(newNote)
+        })
+    })
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        const notes = JSON.parse(data)
+        const updatedNotes = notes.filter(note => note.id !== req.params.id)
+        fs.writeFile('./db/db.json', JSON.stringify(updatedNotes), (err) => {
+            res.send(200)
+        })
+    })
+})
 
 
 
-app.listen(PORT, () =>
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+    );
+    
+
+
+
+
+
+
+    app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
 );
